25ec9285c6bb41e48a93b96283e7adadae0a701d
Jon Schewe <jpschewe@mtu.net>
2011-07-14 18:36:03 -0500

    Put try/catch around program to catch errors.

--- contact-export/export-contacts.cpp
+++ contact-export/export-contacts.cpp
@@ -34,6 +34,7 @@
 #include <iostream>
 #include <cstring>
 #include <sstream>
+#include <exception>
 
 #include "ContactsHeader.hpp"
 
@@ -394,75 +395,80 @@ void output_contact(ofstream *output, ContactsHeader *header,
 }
 
 int main(int argc, char **argv) {
-	pi_file_t *pf;
-	struct DBInfo info;
+	try {
+		pi_file_t *pf;
+		struct DBInfo info;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s [.pdb file]\n", *argv);
-		return 1;
-	}
-
-	if ((pf = pi_file_open(*(argv + 1))) == NULL) {
-		perror("pi_file_open");
-		return 1;
-	}
-
-	pi_file_get_info(pf, &info);
-
-	void *app_info;
-	size_t app_info_size;
-
-	pi_file_get_app_info(pf, &app_info, &app_info_size);
-	if (app_info == NULL) {
-		throw "Unable to get app info";
-	}
-
-	pi_buffer_t *pi_buf = pi_buffer_new(0);
-	pi_buf->data = static_cast<unsigned char *> (app_info);
-	pi_buf->used = app_info_size;
-	pi_buf->allocated = app_info_size;
-
-	struct ContactAppInfo cai;
-	const int result = unpack_ContactAppInfo(&cai, pi_buf);
-	if (-1 == result) {
-		cerr << "Error unpacking contact app info" << endl;
-		return 1;
-	}
-	ContactsHeader header(&cai);
-
-	int nentries;
-	pi_file_get_entries(pf, &nentries);
-	cout << "Number of entries: " << nentries << endl;
-
-	ofstream output("google-contacts.csv", ios::trunc);
-	output_header(&output);
-	for (int entnum = 0; entnum < nentries; entnum++) {
-		unsigned char *buf;
-		int attrs, cat;
-		size_t size;
-		recordid_t uid;
-		if (pi_file_read_record(pf, entnum, (void **) &buf, &size, &attrs,
-				&cat, &uid) < 0) {
-			printf("Error reading record number %d\n", entnum);
-			continue;
+		if (argc != 2) {
+			fprintf(stderr, "Usage: %s [.pdb file]\n", *argv);
+			return 1;
 		}
 
-		pi_buffer_t *pi_buf = pi_buffer_new(size);
-		pi_buffer_append(pi_buf, buf, size);
+		if ((pf = pi_file_open(*(argv + 1))) == NULL) {
+			perror("pi_file_open");
+			return 1;
+		}
 
-		struct Contact contact;
-		const int result = unpack_Contact(&contact, pi_buf, cai.type);
+		pi_file_get_info(pf, &info);
+
+		void *app_info;
+		size_t app_info_size;
+
+		pi_file_get_app_info(pf, &app_info, &app_info_size);
+		if (app_info == NULL) {
+			throw "Unable to get app info";
+		}
+
+		pi_buffer_t *pi_buf = pi_buffer_new(0);
+		pi_buf->data = static_cast<unsigned char *> (app_info);
+		pi_buf->used = app_info_size;
+		pi_buf->allocated = app_info_size;
+
+		struct ContactAppInfo cai;
+		const int result = unpack_ContactAppInfo(&cai, pi_buf);
 		if (-1 == result) {
-			printf("Error unpacking record %d!\n", entnum);
-			continue;
+			cerr << "Error unpacking contact app info" << endl;
+			return 1;
 		}
+		ContactsHeader header(&cai);
 
-		output_contact(&output, &header, &contact, cat);
+		int nentries;
+		pi_file_get_entries(pf, &nentries);
+		cout << "Number of entries: " << nentries << endl;
 
+		ofstream output("google-contacts.csv", ios::trunc);
+		output_header(&output);
+		for (int entnum = 0; entnum < nentries; entnum++) {
+			unsigned char *buf;
+			int attrs, cat;
+			size_t size;
+			recordid_t uid;
+			if (pi_file_read_record(pf, entnum, (void **) &buf, &size, &attrs,
+					&cat, &uid) < 0) {
+				printf("Error reading record number %d\n", entnum);
+				continue;
+			}
+
+			pi_buffer_t *pi_buf = pi_buffer_new(size);
+			pi_buffer_append(pi_buf, buf, size);
+
+			struct Contact contact;
+			const int result = unpack_Contact(&contact, pi_buf, cai.type);
+			if (-1 == result) {
+				printf("Error unpacking record %d!\n", entnum);
+				continue;
+			}
+
+			output_contact(&output, &header, &contact, cat);
+
+		}
+		output.close();
+
+		pi_file_close(pf);
+
+		return 0;
+	} catch (exception &e) {
+		cerr << "Unexpected error: " << e.what() << endl;
+		return 1;
 	}
-	output.close();
-
-	pi_file_close(pf);
-
-	return 0;
 }
